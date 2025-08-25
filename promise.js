ab5b9394a1bf7d94375cdfa739f2aab37c9e5aca
Evo-32 <evodieumuhuza@gmail.com>
2025-08-25 12:33:02 +0200

    the simple promise

---
+++ promise.js
@@ -0,0 +1,26 @@
+// let p = new Promise((resolve,reject) => {
+//     let a = 1 + 2
+//     if(a == 2){
+//         resolve('success')
+//     }else{
+//         reject('failed')
+//     }
+// })
+// p.then((message)=>{
+//     console.log('this is in  then ' + message)
+// }).catch((message) => {
+//     console.log('this is in  catch ' + message)
+// })
+
+
+// let Greeting = new Promise((resolve,reject) => {
+//     setTimeout((){
+//         resolve('Hello,  world!')
+//     },2000)
+// })
+// Greeting.then((message) => {
+//     console.log(message)
+// })
+
+
+
