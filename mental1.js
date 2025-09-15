180a32cddb4e3c158b71593d44a500a3d9766d15
Evo-32 <evodieumuhuza@gmail.com>
2025-09-15 19:56:11 +0200

    class and generator

--- mental1.js
+++ mental1.js
@@ -30,19 +30,69 @@
 // console.log(assignId(person))
 
 
-const names = (arr) =>{
-  let result = []
-  for(const {name,age} of arr){
-    if(age > 18){
-       result.push(name.toUpperCase())
-    }
-  }
-  return result
-}
-const people = [
-  { name: 'Alice', age: 16 },
-  { name: 'Bob', age: 22 },
-  { name: 'Charlie', age: 17 },
-  { name: 'David', age: 25 },
-];
-console.log(names(people))
+// const names = (arr) =>{
+//   let result = []
+//   for(const {name,age} of arr){
+//     if(age > 18){
+//        result.push(name.toUpperCase())
+//     }
+//   }
+//   return result
+// }
+// const people = [
+//   { name: 'Alice', age: 16 },
+//   { name: 'Bob', age: 22 },
+//   { name: 'Charlie', age: 17 },
+//   { name: 'David', age: 25 },
+// ];
+// console.log(names(people))
+
+
+// const uniques = (arr) =>{
+//   return [...new Set(arr.flat(2).sort((a,b) => a - b ))]
+// }
+// console.log(uniques([1, [2, 3], 4, [2, [5, 1]]]))
+
+
+// const extractWordsAndNumbers = arr =>
+//   arr.flatMap(str => {
+//     const letters = str.split('').filter(c => isNaN(c) || c === ' ').join('');
+//     const numbers = Number(str.split('').filter(c => !isNaN(c)).join(''));
+//     return letters ? [letters, numbers] : [numbers];
+//   });
+// const input = ["Hello123", "World456", "49", "Another789"];
+// const output = extractWordsAndNumbers(input);
+// console.log(output);
+
+
+// function* fibonacciGenerator(limit) {
+//   let a = 0, b = 1;
+//   while (a <= limit) {
+//     yield a;
+//     let temp = a;
+//     a = b;
+//     b = temp + b;
+//   }
+// }
+// console.log([... fibonacciGenerator(20)])
+
+
+// class Product {
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+//   getDetails() {
+//     return `${this.name} costs $${this.price.toFixed(2)}`;
+//   }
+//   applyDiscount(discount) {
+//     const discountedPrice = this.price * (1 - discount / 100);
+//     this.price = discountedPrice;
+//     return discountedPrice;
+//   }
+// }
+// const prod = new Product("Laptop", 1000);
+// console.log(prod.getDetails());  
+// console.log(prod.applyDiscount(10)); 
+// console.log(prod.getDetails());      
+
